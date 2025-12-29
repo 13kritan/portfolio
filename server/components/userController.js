c034975564c7ac074f36ccb7014cad70223c04f6
13kritan <13kritan@gmail.com>
2025-12-29 21:21:04 +0545

    Update userController.js

--- server/components/userController.js
+++ server/components/userController.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 
 module.exports.sendEmail = async (req, res) => {
     const { firstname, lastname, email, subject, message } = req.body
-
+    console.log(firstname, lastname)
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
