39acc732f450200c7c9bb4dc5436169e6836faf1
13kritan <13kritan@gmail.com>
2026-05-17 22:16:06 +0545

    Update ContactInfoBox.jsx

--- public/src/components/ContactInfoBox.jsx
+++ public/src/components/ContactInfoBox.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export default function ContactInfoBox({ icon, header, info}) {
     return (
