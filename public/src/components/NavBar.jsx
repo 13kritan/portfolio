8b69869048b7116b71ff85a3f8048b6439cfbda6
13kritan <13kritan@gmail.com>
2026-05-17 23:15:31 +0545

    Update NavBar.jsx

--- public/src/components/NavBar.jsx
+++ public/src/components/NavBar.jsx
@@ -63,7 +63,7 @@ export default function NavBar({ isHire, handleHireClick }) {
             :
             !isHire ?
               <div className='hidden md:block vsm:block cursor-pointer text-lg px-2 py-1' onClick={handleMenuClick} ><FontAwesomeIcon icon={faMenorah} /></div> :
-              <div className={`${isHire ? 'hidden' : ''}`}>
+              <div className={`${isHire ? 'hidden  md:block vsm:block' : ''}`}>
                 <ButtonDisplay handleSubmit={handleHireClick} buttonName={!isHire ? "Hire Me" : "Home"} /> 
               </div>
 
